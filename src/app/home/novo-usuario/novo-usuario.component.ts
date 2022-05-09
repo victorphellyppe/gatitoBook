403b7d8f0c8a6d0ab7113cdfab95e5449ad54c03
Victor Oliveira <victor.oliveira@blueservice.com.br>
2022-05-09 09:47:56 -0300

    Validação de formulário padrão!

--- src/app/home/novo-usuario/novo-usuario.component.ts
+++ src/app/home/novo-usuario/novo-usuario.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NovoUsuarioService } from './novo-usuario.service';
 import { NovoUsuario } from './novo-usuario.ts';
 
@@ -14,9 +14,9 @@ export class NovoUsuarioComponent implements OnInit {
 
   ngOnInit(): void {
     this.novoUsuarioForm = this.formBuilder.group({
+      email:    ['', [Validators.required, Validators.email]],
+      fullName: ['', [Validators.required, Validators.minLength(4)]],
       userName: [''],
-      email:    [''],
-      fullName: [''],
       password: ['']
     });
   }
