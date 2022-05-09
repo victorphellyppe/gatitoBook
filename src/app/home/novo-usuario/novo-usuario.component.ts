1ae8de129c24dd9a1f0b6b1081f256ef4221fb84
Victor Oliveira <victor.oliveira@blueservice.com.br>
2022-05-09 10:01:39 -0300

    Validações customizadas, com angular!

--- src/app/home/novo-usuario/novo-usuario.component.ts
+++ src/app/home/novo-usuario/novo-usuario.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { minusculoValidator } from './minusculo';
 import { NovoUsuarioService } from './novo-usuario.service';
 import { NovoUsuario } from './novo-usuario.ts';
 
@@ -16,7 +17,7 @@ export class NovoUsuarioComponent implements OnInit {
     this.novoUsuarioForm = this.formBuilder.group({
       email:    ['', [Validators.required, Validators.email]],
       fullName: ['', [Validators.required, Validators.minLength(4)]],
-      userName: [''],
+      userName: ['', [minusculoValidator]],
       password: ['']
     });
   }
